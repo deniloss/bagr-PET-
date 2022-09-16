f1cdf0c8a0e84382bb2f8a2709f2ea150076c016
DaniilSitnik <danil.sitnik.work@gmail.com>
2022-09-16 14:05:15 +0300

    feat: project's theme switcher

---
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "app/providers/ThemeProvider/lib/ThemeContext";
+
+const ThemeProvider: FC = ({children}) => {
+
+  const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+
+  const [theme, setTheme] = React.useState<Theme>(defaultTheme)
+
+  const defaultProps = React.useMemo(() =>({
+    theme: theme,
+    setTheme: setTheme
+  }), [theme])
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
