6445257c57549502098f8ac6fa7e33fb0ac72a1e
deniloss <danil.sitnik.it@yandex.ru>
2022-09-20 19:08:26 +0300

    feat: added Babel to webpack config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -15,6 +15,17 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
+  const babelLoader = {
+    test: /\.(jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  }
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -35,18 +46,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   }
 
   const fileLoader = {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    }
-
-    return [
-      typescriptLoader,
-      scssLoader,
-      svgLoader,
-      fileLoader,
-    ]
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
   }
+
+  return [
+    babelLoader,
+    typescriptLoader,
+    scssLoader,
+    svgLoader,
+    fileLoader,
+  ]
+}
