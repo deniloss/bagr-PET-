c6372d5f7fd4ef29036e1b2bc2a8c86d6b2d9798
deniloss <danil.sitnik.it@yandex.ru>
2022-09-27 02:15:50 +0300

    jest: edited config for absolute paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
-    'max-len': ['warn', { ignoreComments: true }],
+    'max-len': ['warn', { code: 100, ignoreComments: true }],
   },
   globals: {
     __IS_DEV__: true,
--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -22,6 +22,10 @@ export default {
   moduleDirectories: [
     'node_modules',
   ],
+  modulePaths: [
+    '<rootDir>',
+    'src',
+  ],
   moduleFileExtensions: [
     'js',
     'jsx',
