582dedabcb392303ada8d74fa8693b53fc2ca9d1
deniloss <danil.sitnik.it@yandex.ru>
2022-09-28 12:51:02 +0300

    i18n: added translations

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,22 +2,26 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { BugButton } from 'app/providers/ErrorBoundary/ui/BugButton/BugButton';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(cls.Navbar, {}, [className])}>
-    <div className={cls.links}>
-      <BugButton />
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-        Главная
-      </AppLink>
-      <AppLink theme={AppLinkTheme.RED} to="/about">
-        О сайте
-      </AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <BugButton />
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+          {t('Главная')}
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to="/about">
+          {t('О сайте')}
+        </AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
