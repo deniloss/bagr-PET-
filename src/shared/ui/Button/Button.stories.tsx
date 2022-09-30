b2b48bf68134b2f73ec05064ea0d2247aa39b31c
deniloss <danil.sitnik.it@yandex.ru>
2022-09-30 13:07:14 +0300

    shared: added new storybooks for Button.tsx, loki tests approved + GitHub Actions updated

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Button, ThemeButton } from './Button';
+import { Button, ButtonSize, ThemeButton } from './Button';
 
 export default {
   title: 'shared/Button',
@@ -30,6 +30,21 @@ export const Outline = Template.bind({});
 Outline.args = {
   children: 'Text',
   theme: ThemeButton.OUTLINE,
+  size: ButtonSize.M,
+};
+
+export const OutlineL = Template.bind({});
+OutlineL.args = {
+  children: 'Text',
+  theme: ThemeButton.OUTLINE,
+  size: ButtonSize.L,
+};
+
+export const OutlineXL = Template.bind({});
+OutlineXL.args = {
+  children: 'Text',
+  theme: ThemeButton.OUTLINE,
+  size: ButtonSize.XL,
 };
 
 export const OutlineDark = Template.bind({});
@@ -38,3 +53,27 @@ OutlineDark.args = {
   theme: ThemeButton.OUTLINE,
 };
 OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const SquareM = Template.bind({});
+SquareM.args = {
+  children: '>',
+  theme: ThemeButton.BACKGROUND_INVERTED,
+  size: ButtonSize.M,
+  square: true,
+};
+
+export const SquareL = Template.bind({});
+SquareL.args = {
+  children: '>',
+  theme: ThemeButton.BACKGROUND_INVERTED,
+  size: ButtonSize.L,
+  square: true,
+};
+
+export const SquareXL = Template.bind({});
+SquareXL.args = {
+  children: '>',
+  theme: ThemeButton.BACKGROUND_INVERTED,
+  size: ButtonSize.XL,
+  square: true,
+};
