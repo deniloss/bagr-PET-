291f162ee03badab62f08eac77b404c22d8ff5bb
deniloss <danil.sitnik.it@yandex.ru>
2022-10-03 20:18:20 +0300

    shared: created ui component Modal.tsx

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Portal } from 'shared/ui/Portal/Portal';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+  className?: string
+  children?: ReactNode
+  isOpen: boolean
+  onClose: () => void
+}
+
+const ANIMATION_DELAY = 200;
+
+export const Modal = (props: ModalProps) => {
+  const {
+    className,
+    children,
+    isOpen,
+    onClose,
+  } = props;
+
+  const [isClosing, setIsClosing] = React.useState(false);
+  const timeRef = React.useRef<ReturnType<typeof setTimeout>>();
+
+  const mods: Record<string, boolean> = {
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing,
+  };
+
+  const contentClickHandler = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  const closeHandler = React.useCallback(() => {
+    if (onClose) {
+      setIsClosing(true);
+      timeRef.current = setTimeout(() => {
+        onClose();
+        setIsClosing(false);
+      }, ANIMATION_DELAY);
+    }
+  }, [onClose]);
+
+  const onKeyDown = React.useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      closeHandler();
+    }
+  }, [closeHandler]);
+
+  React.useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown);
+    }
+
+    return () => {
+      clearTimeout(timeRef.current);
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [isOpen, onKeyDown]);
+
+  return (
+    <Portal>
+      <div className={classNames(cls.Modal, mods, [className])}>
+        <div
+          className={cls.overlay}
+          onClick={closeHandler}
+        >
+          <div
+            className={classNames(cls.content, { [cls.contentOpened]: isOpen }, [className])}
+            onClick={contentClickHandler}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </Portal>
+  );
+};
