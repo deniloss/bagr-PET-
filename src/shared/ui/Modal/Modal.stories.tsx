1c6dd3a8743a5793b94d23ede47b14e36689acff
deniloss <danil.sitnik.it@yandex.ru>
2022-10-04 00:16:15 +0300

    storybook: added books for Modal.tsx

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Modal } from './Modal';
+
+export default {
+  title: 'shared/Modal',
+  component: Modal,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isOpen: true,
+  children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus deleniti eum incidunt laborum minima modi perspiciatis quo sint voluptate.\n',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  isOpen: true,
+  children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus deleniti eum incidunt laborum minima modi perspiciatis quo sint voluptate.\n',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
