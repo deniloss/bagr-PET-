4f57f8f80539c50dc5fb4a81491e92637d4c8f81
deniloss <danil.sitnik.it@yandex.ru>
2022-10-10 15:02:57 +0300

    features: added public LoginModal

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import { Input } from 'shared/ui/Input/Input';
+import cls from './LoginForm.module.scss';
+
+interface LoginFormProps {
+  className?: string
+}
+
+export const LoginForm = ({ className }: LoginFormProps) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(cls.LoginForm, {}, [className])}>
+      <Input
+        type="text"
+        className={cls.inputs}
+        placeholder={t('Ваше имя')}
+      />
+      <Input
+        type="text"
+        className={cls.inputs}
+        placeholder={t('Пароль')}
+      />
+      <Button className={cls.LoginBtn}>
+        {t('Войти')}
+      </Button>
+    </div>
+  );
+};
