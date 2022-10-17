614c632ee6374f81e6c80d2c18451e8557dd3021
deniloss <danil.sitnik.it@yandex.ru>
2022-10-17 19:15:35 +0300

    providers: added typed hook useAppDispatch

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,7 +1,8 @@
 import { StoreProvider } from './ui/StoreProvider';
-import { createStore } from './config/store';
+import { AppDispatch, createStore } from './config/store';
 
 export {
   createStore,
   StoreProvider,
+  AppDispatch,
 };
