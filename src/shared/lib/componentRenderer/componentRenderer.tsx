71f4bcfd27e78e821957f457b62aaba09918b02d
deniloss <danil.sitnik.it@yandex.ru>
2022-10-22 19:12:47 +0300

    unit tests: fixed errors inside tests

--- src/shared/lib/componentRenderer/componentRenderer.tsx
+++ src/shared/lib/componentRenderer/componentRenderer.tsx
@@ -4,7 +4,6 @@ import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTest';
 import { MemoryRouter } from 'react-router-dom';
 import { StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 
 export interface componentRenderOptions {
@@ -19,12 +18,12 @@ export function componentRenderer(component: ReactNode, options: componentRender
   } = options;
 
   return render(
-    <StoreProvider initialState={initialState}>
-      <MemoryRouter initialEntries={[route]}>
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider initialState={initialState}>
         <I18nextProvider i18n={i18nForTests}>
           {component}
         </I18nextProvider>
-      </MemoryRouter>
-    </StoreProvider>,
+      </StoreProvider>
+    </MemoryRouter>,
   );
 }
