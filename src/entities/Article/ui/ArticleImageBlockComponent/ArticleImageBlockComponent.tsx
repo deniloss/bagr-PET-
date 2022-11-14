14a9d2a4585799fbae5ba9e0326f29d48f28ea7c
deniloss <danil.sitnik.it@yandex.ru>
2022-11-14 13:34:27 +0300

    entities: added ui for Article entity

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
+import { ArticleImageBlock } from 'entities/Article/model/types/article';
+import { Text, TextAlign, TextSize } from 'shared/ui/Text/Text';
 import cls from './ArticleImageBlockComponent.module.scss';
 
 interface ArticleImageBlockComponentProps {
   className?: string
-  image: string
+  block: ArticleImageBlock
 }
 
-export const ArticleImageBlockComponent = ({ className, image }: ArticleImageBlockComponentProps) => {
-  const { t } = useTranslation();
-  return (
-    <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
-      {/* eslint-disable-next-line i18next/no-literal-string */}
-      <img className={cls.articleImage} alt="img" src={image} />
-    </div>
-  );
-};
+export const ArticleImageBlockComponent = ({ className, block }: ArticleImageBlockComponentProps) => (
+  <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
+    {/* eslint-disable-next-line i18next/no-literal-string */}
+    <img className={cls.articleImage} alt="img" src={block.src} />
+    <Text text={block.title} align={TextAlign.CENTER} size={TextSize.M} />
+  </div>
+);
