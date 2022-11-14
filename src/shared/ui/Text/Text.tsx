23e398917da589fbc8d89d64d0b94001a1bf44de
deniloss <danil.sitnik.it@yandex.ru>
2022-11-14 13:20:19 +0300

    shared: added textSizes for Text.ts

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -13,12 +13,18 @@ export enum TextAlign {
   LEFT = 'left'
 }
 
+export enum TextSize {
+  M = 'size_m',
+  L = 'size_l'
+}
+
 interface TextProps {
   className?: string
   title?: string
   text?: string
   align?: TextAlign
   theme?: TextTheme
+  size?: TextSize
 }
 
 export const Text = memo((props: TextProps) => {
@@ -28,9 +34,11 @@ export const Text = memo((props: TextProps) => {
     text,
     align = TextAlign.LEFT,
     theme = TextTheme.PRIMARY,
+    size = TextSize.M,
   } = props;
+
   return (
-    <div className={classNames(cls.Text, { [cls[theme]]: true, [cls[align]]: true }, [className])}>
+    <div className={classNames(cls.Text, { [cls[theme]]: true, [cls[align]]: true, [cls[size]]: true }, [className])}>
       {title && <p className={cls.title}>{title}</p>}
       {text && <p className={cls.text}>{text}</p>}
     </div>
