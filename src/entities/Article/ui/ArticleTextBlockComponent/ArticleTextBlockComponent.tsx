1e2a4bcc3f9d5d757126b710176fe6f600459964
deniloss <danil.sitnik.it@yandex.ru>
2022-12-14 14:09:06 +0300

    entities: added ImageBlock + fixed problem with width of TextBlock

--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { Text } from 'shared/ui/Text/Text';
+import { ArticleTextBlock } from 'entities/Article/model/types/article';
 import cls from './ArticleTextBlockComponent.module.scss';
 
 interface ArticleTextBlockComponentProps {
   className?: string
-  paragraphs: string[]
+  block: ArticleTextBlock
 }
 
-export const ArticleTextBlockComponent = ({ className, paragraphs }: ArticleTextBlockComponentProps) => {
-  const { t } = useTranslation();
-  return (
-    <div className={classNames(cls.ArticleTextBlockComponent, {}, [className])}>
-      {paragraphs.map((item) => <Text className={cls.textBlock} text={item} />)}
-    </div>
-  );
-};
+export const ArticleTextBlockComponent = ({ className, block }: ArticleTextBlockComponentProps) => (
+  <div className={classNames(cls.ArticleTextBlockComponent, {}, [className])}>
+    {block.paragraphs.map((item) => <Text className={cls.textBlock} text={item} />)}
+  </div>
+);
