1ecd75d270d2e6405dbba3c5690cd05f2083c075
deniloss <danil.sitnik.it@yandex.ru>
2022-12-18 19:02:53 +0300

    added storybooks for new components

--- src/entities/Comment/ui/CommentList/CommentList.stories.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { CommentList } from './CommentList';
 
 export default {
-  title: 'shared/CommentList',
+  title: 'entities/CommentList',
   component: CommentList,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -13,4 +15,81 @@ export default {
 const Template: ComponentStory<typeof CommentList> = (args) => <CommentList {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  comments: [
+    {
+      user: {
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        id: '1',
+        username: 'denilos',
+      },
+      id: '1',
+      text: 'text text',
+    },
+    {
+      user: {
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        id: '2',
+        username: 'Yan',
+      },
+      id: '1',
+      text: 'like this',
+    },
+  ],
+};
+
+export const DarkTheme = Template.bind({});
+DarkTheme.args = {
+
+  comments: [
+    {
+      user: {
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        id: '1',
+        username: 'denilos',
+      },
+      id: '1',
+      text: 'text text',
+    },
+    {
+      user: {
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        id: '2',
+        username: 'Yan',
+      },
+      id: '1',
+      text: 'like this',
+    },
+  ],
+};
+DarkTheme.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const isLoading = Template.bind({});
+isLoading.args = {
+  isLoading: true,
+  comments: [
+    {
+      user: {
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        id: '1',
+        username: 'denilos',
+      },
+      id: '1',
+      text: 'text text',
+    },
+    {
+      user: {
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        id: '2',
+        username: 'Yan',
+      },
+      id: '1',
+      text: 'like this',
+    },
+  ],
+};
+
+export const emptyList = Template.bind({});
+emptyList.args = {
+  comments: [],
+};
--- src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import AddCommentForm from './AddCommentForm';
 
 export default {
-  title: 'shared/AddCommentForm',
+  title: 'features/AddCommentForm',
   component: AddCommentForm,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -16,3 +17,22 @@ const Template: ComponentStory<typeof AddCommentForm> = (args) => <AddCommentFor
 
 export const Primary = Template.bind({});
 Primary.args = {};
+Primary.decorators = [StoreDecorator({
+  CommentForm: {
+    isLoading: false,
+    error: '',
+    text: 'comments',
+  },
+})];
+
+export const DarkTheme = Template.bind({});
+DarkTheme.args = {};
+DarkTheme.decorators = [
+  StoreDecorator({
+    CommentForm: {
+      isLoading: false,
+      error: '',
+      text: 'comments',
+    },
+  }),
+  ThemeDecorator(Theme.DARK)];
