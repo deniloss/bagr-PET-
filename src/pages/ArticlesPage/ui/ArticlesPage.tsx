199865086ce847545b546502c8833a6057555b23
deniloss <danil.sitnik.it@yandex.ru>
2022-12-23 18:10:10 +0300

    fixed problem with rendering skeletons (mistake in selectors)

--- src/pages/ArticlesPage/ui/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage.tsx
@@ -4,15 +4,15 @@ import { useTranslation } from 'react-i18next';
 import { ArticleList } from 'entities/Article';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { fetchArticles } from 'pages/ArticlesPage/model/services/fetchArticles/fetchArticles';
 import { useSelector } from 'react-redux';
 import { articlesPageActions, getArticles } from 'pages/ArticlesPage/model/slice/articlesPageSlice';
 import { ArticleViewSelector } from 'features/ArticleViewSelector';
-import { getArticlesPageView } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
+import { getArticlesPageIsLoading, getArticlesPageView } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
 import { ArticleView } from 'entities/Article/model/types/article';
 import { getArticleIsLoading } from 'entities/Article/model/selectors/Article';
 import { Page } from 'shared/ui/Page/Page';
 import { fetchNextArticlesPage } from 'pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage';
+import { fetchArticles } from 'pages/ArticlesPage/model/services/fetchArticles/fetchArticles';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -24,7 +24,7 @@ const ArticlesPage = ({ className }: ArticlesPageProps) => {
   const dispatch = useAppDispatch();
   const articles = useSelector(getArticles.selectAll);
   const view = useSelector(getArticlesPageView);
-  const isLoading = useSelector(getArticleIsLoading);
+  const isLoading = useSelector(getArticlesPageIsLoading);
 
   useInitialEffect(() => {
     dispatch(articlesPageActions.initState());
